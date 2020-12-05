5d70237d92787933401b3f7cbcfaa3ce38e92b5e
Dipali <balasdeep@gmail.com>
2020-12-05 18:22:32 +0530

    your commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var url_val = document.getElementById("url").innerText;
+
+function submit(){
+  var temp_url = url_val;
+  console.log("url:"+ url_val);
+  var name_val = document.getElementById("name").value;
+  console.log(name_val);
+
+  var year_val = document.getElementById("year").value;
+  console.log(year_val);
+
+  if(name_val !== "")
+  {
+    temp_url += "?name="+name_val;
+  }
+  if (year_val !== "")
+  {
+    temp_url += "&year="+year_val;
+  }
+
+  document.getElementById("url").innerText = temp_url;
+}
